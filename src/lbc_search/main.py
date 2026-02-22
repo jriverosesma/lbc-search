77c1b2116069c3a0763fcac74f3583da386fd1ea
jriverosesma <jriverosesma@gmail.com>
2026-02-22 22:19:19 +0100

    feat(sort & limit): implement better limit option and sort option

--- src/lbc_search/main.py
+++ src/lbc_search/main.py
@@ -31,18 +31,18 @@ def main() -> None:
         help="LeBonCoin search URL to query (copy/paste the full URL from your browser).",
     )
     parser.add_argument(
-        "--page",
-        "-p",
-        type=int,
-        default=1,
-        help="Results page number to fetch (starts at 1).",
-    )
-    parser.add_argument(
         "--limit",
         "-l",
         type=int,
-        default=999,
-        help="Maximum number of ads to return (depends on what the API supports).",
+        default=100,
+        help="Maximum number of ads to return.",
+    )
+    parser.add_argument(
+        "--sort",
+        "-s",
+        type=str,
+        default="relevance",
+        help=f"Sorting type. Available values are: {[member.name.lower() for member in lbc.enums.Sort]} (default: 'relevance').",
     )
     parser.add_argument(
         "--out",
@@ -54,34 +54,48 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    search_and_export(url=args.url, page=args.page, limit=args.limit, out=args.out)
-
-
-def search_and_export(url: str, page: int, limit: int, out: Path) -> None:
-    client = lbc.Client()
-
-    result = client.search(
-        url=url,
-        page=page,
-        limit=limit,
+    search_and_export(
+        url=args.url,
+        limit=args.limit,
+        sort=lbc.enums.Sort[args.sort.upper()],
+        out=args.out,
     )
 
-    results = []
-    for add in result.ads:
-        results.append(
-            AddRelevantFields(
-                title=add.title,
-                description=add.body,
-                date=add.index_date,
-                price=add.price,
-                user_score=add.user.feedback.score,
-                nb_user_evaluations=add.user.feedback.received_count,
-                url=add.url,
-            ).to_dict()
-        )
 
+def search_and_export(url: str, limit: int, sort: lbc.enums.Sort, out: Path) -> None:
+    # Search
+    client = lbc.Client()
+    preliminary_search = client.search(url=url, limit=1, sort=sort)
+
+    results = []
+    nb_pages = preliminary_search.max_pages
+    add_count = 0
+    for page in range(1, nb_pages + 1):
+        if add_count >= limit:
+            break
+        search = client.search(url=url, page=page)
+        for add in search.ads:
+            results.append(
+                AddRelevantFields(
+                    title=add.title,
+                    description=add.body,
+                    date=add.index_date,
+                    price=add.price,
+                    user_score=add.user.feedback.score,
+                    nb_user_evaluations=add.user.feedback.received_count,
+                    url=add.url,
+                ).to_dict()
+            )
+            add_count += 1
+            if add_count >= limit:
+                break
+
+    # Export
     with open(out, mode="w", encoding="utf-8") as f:
         json.dump(results, f, indent=4, ensure_ascii=False, default=str)
+
+    # Display results summary
+    print(f"Successfully retrieved and exported {len(results)} results to {out}")
 
 
 if __name__ == "__main__":
